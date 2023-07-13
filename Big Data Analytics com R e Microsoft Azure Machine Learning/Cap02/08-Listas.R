3e117c15e8dabe363fb2e6060c7b36bbf424006e
Gustavo Zarpelon Radaelli <gustavogzr@hotmail.com>
2023-07-13 02:02:32 -0300

    Aula 01

---
+++ Big Data Analytics com R e Microsoft Azure Machine Learning/Cap02/08-Listas.R
@@ -0,0 +1,13 @@
+# Configurando o diretório de trabalho
+setwd("C:/Users/gusta/DSA-Data-Science/Big Data Analytics com R e Microsoft Azure Machine Learning/Cap02")
+getwd()
+
+# Use list() para criar uma lista
+
+# Lista de strings
+lista_caracter1 = list('A', 'B', 'C')
+lista_caracter1
+lista_caracter2 = list(c('A','A'), 'B', 'C')
+lista_caracter2
+lista_caracter3 = list(matrix(c('A','A','A','A'), nr = 2), 'B', 'C')
+lista_caracter3
